46954b72c0f66d060cb69fe4ad0159e27a4a3284
Paulo Gonçalves <94994578+Paulo-gsilva@users.noreply.github.com>
2022-02-22 17:26:03 -0300

    Update main.js
    Atualizando. Commits mais detalhados no repositório 'meu-portfolio'.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,40 @@
 const login = document.querySelector('.button-login');
+const buttonLoginOpen = document.querySelector('.button-login-open');
 const sign = document.querySelector('.button-sign');
+const buttonSignRegister = document.querySelector('.button-sign-register');
 const buttonReturn = document.querySelector('.button-return');
 const buttonReturnSign = document.querySelector('.button-return-sign');
+const cpf = document.querySelector('.form-sign-cpf');
+const phone = document.getElementById('number');
+
+phone.addEventListener('keypress', function(){
+    let phoneLength = phone.value.length;
+    
+    if(phoneLength === 0){
+        phone.value += '(';
+    }
+    else if(phoneLength === 3){
+        phone.value += ')';
+    }
+    else if(phoneLength === 5){
+        phone.value += ' ';
+    }
+    else if(phoneLength === 10){
+        phone.value += '-';
+    }
+
+});
+
+cpf.addEventListener('keypress', function(){
+    let cpfLength = cpf.value.length;
+    if(cpfLength === 3 || cpfLength === 7){
+        cpf.value += '.';
+    }
+    else if(cpfLength === 11){
+        cpf.value += '-';
+    }
+
+});
 
 function functionRemoveLogin(){
     const inputButtons = document.querySelector('.input-buttons');
@@ -62,11 +95,89 @@ function functionShowFormSign(){
     functionButtonReturnSign();
 }
 
-login.addEventListener('click', function(event){    
-    functionRemoveLogin();
+function registerNewUser(){
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let number = document.getElementById('number');
+    let cpf = document.getElementById('cpf');
+    let birth = document.getElementById('birth');
+
+    let data = JSON.parse(localStorage.getItem('dataUsers')); //LENDO DADOS ARMAZENADOS
+
+    if(data == null){
+        alert('Por favor, tente Novamente')
+        localStorage.setItem('dataUsers', '[]');
+    }
+
+    let saveData = {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        number: number.value,
+        cpf: cpf.value,
+        birth: birth.value
+    }
+
+    data.push(saveData);
+    localStorage.setItem('dataUsers', JSON.stringify(data)); //ARMAZENANDO DADOS
+    
+}
+
+function emailVerify(textEmail){ //VERIFICA EMAILS EXISTENTES
+    let data = JSON.parse(localStorage.getItem('dataUsers'));
+
+    const emailVerify = data.filter(function(email){
+        return email.email === textEmail;
+    });
+
+    return emailVerify;
+}
+
+function passwordVerify(textPassword){ //VERIFICA SENHAS EXISTENTES
+    let data = JSON.parse(localStorage.getItem('dataUsers'));
+
+    const passwordVerify = data.filter(function(password){
+        return password.password === textPassword;
+    });
+
+    return passwordVerify;
+}
+
+function loginVerify(textEmail, textPassword){
+    const email = emailVerify(textEmail);
+    const password = passwordVerify(textPassword);
+
+    function loopEmail(){
+        for(let index of email){
+            return index.name;
+        }
+    }
+
+    function loopPassword(){
+        for(let indexPass of password){
+            return indexPass.name;
+        }
+    }
+
+    let result = loopEmail() === loopPassword() ? alert('Login Efetuado Com Sucesso') : alert('Email ou Senha Incorreto');
+}
+
+buttonLoginOpen.addEventListener('click', (event) => {
+    let email = document.querySelector('.email-login');
+    let password = document.querySelector('.password-login');
+
+    event.preventDefault();
+    loginVerify(email.value, password.value);
+    
 });
 
-sign.addEventListener('click', function(event){
-    functionRemoveAllFromContainer();
+buttonSignRegister.addEventListener('click', (event) => {
+    event.preventDefault();
+    registerNewUser();
+    alert('Conta Registrada Com Sucesso')
 });
+
+login.addEventListener('click', (event) => functionRemoveLogin());
+sign.addEventListener('click', (event) => functionRemoveAllFromContainer());
 
